e909d79bee40d9eb49b8d54b03722d489ed1bb4a
marrcelp <marcel.piaszczyk@gmail.com>
2025-04-12 22:47:07 +0200

    resolved exercise 2

--- 02/app.js
+++ 02/app.js
@@ -9,8 +9,15 @@ class Counter extends React.Component {
     }
     
     render() {
-        return <button>click me ({ this.state.amount })</button>
+        return <button onClick={this.handleClick}>click me ({ this.state.amount })</button>
+    }
+
+
+    handleClick = () => {
+        const {amount} = this.state;
+        this.setState({amount: amount+1});
     }
 }
+
 
 root.render(<Counter />);
